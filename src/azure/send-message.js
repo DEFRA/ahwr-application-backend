ba86cd97123d31da010185eb9498074a52c13e06
Ben Hope <bendef24@gmail.com>
2025-10-15 09:59:16 +0100

    feat(AHWR-1178): send message via library, temp logging

--- src/azure/send-message.js
+++ src/azure/send-message.js
@@ -2,7 +2,10 @@ import { createMessageSender } from './create-message-sender.js'
 import { createMessage } from './create-message.js'
 
 export const sendMessage = async (body, type, config, options) => {
+  console.log('BH TEST: starting send via lib')
+  console.log(`BH TEST: config check: ${config.address}`)
   const message = createMessage(body, type, options)
   const sender = createMessageSender(config)
+  console.log('BH TEST: starting send message')
   await sender.sendMessage(message)
 }
