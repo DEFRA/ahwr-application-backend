40eee985f90523d7fbf224f81b7f1cad5cecf27f
Ben Hope <bendef24@gmail.com>
2025-10-15 14:35:53 +0100

    feat(AHWR-1178): send message via library, remove connection string from config

--- src/azure/send-message.js
+++ src/azure/send-message.js
@@ -4,16 +4,5 @@ import { createMessage } from './create-message.js'
 export const sendMessage = async (body, type, config, options) => {
   const message = createMessage(body, type, options)
   const sender = createMessageSender(config)
-
-  // const tempConfig = {
-  //   ...config,
-  //   password: config.password
-  //     ? `${config.password.substring(0, 3)}***`
-  //     : 'not-set-correctly'
-  // }
-  const tempConfig = { ...config, password: '***REDACTED***' }
-  console.log(`BH sender: ${sender}`)
-  console.log(`BH config: ${JSON.stringify(tempConfig)}`)
-  console.log(`BH Message: ${JSON.stringify(message)}`)
   await sender.sendMessage(message)
 }
