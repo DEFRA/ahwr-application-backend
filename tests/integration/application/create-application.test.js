2b9512929670c7cf21ff259041940b26c8795cc8
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-10-23 16:30:30 +0100

    fix(AHWR-1333): integration test with in memory db

--- tests/integration/application/create-application.test.js
+++ tests/integration/application/create-application.test.js
@@ -1,3 +1,43 @@
+import { setupTestEnvironment, teardownTestEnvironment } from '../test-utils.js'
+
 describe('Create application', () => {
-  test.todo('should create an agreement for a brand new user')
+  let server
+
+  beforeAll(async () => {
+    server = await setupTestEnvironment()
+  })
+
+  afterAll(async () => {
+    await teardownTestEnvironment()
+  })
+
+  const options = {
+    method: 'POST',
+    url: '/api/applications',
+    payload: {
+      reference: 'TEMP-5C1C-DD6Z',
+      confirmCheckDetails: 'yes',
+      declaration: true,
+      offerStatus: 'accepted',
+      organisation: {
+        farmerName: 'Mr Farmer',
+        name: 'My Amazing Farm',
+        sbi: '118409263',
+        cph: '123/456/789',
+        crn: '112223',
+        address: '1 Example Road',
+        email: 'business@email.com',
+        userType: 'newUser'
+      }
+    }
+  }
+
+  test('successfully creates a new application', async () => {
+    const res = await server.inject(options)
+
+    expect(res.statusCode).toBe(200)
+    expect(JSON.parse(res.payload)).toEqual({
+      applicationReference: 'IAHW-5C1C-DD6Z'
+    })
+  })
 })
