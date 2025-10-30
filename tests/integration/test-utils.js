15ee1983d78ea9b5bd86af5bc01133ad6a2665c4
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-10-30 11:54:55 +0000

    fix(AHWR-1332): setup integration test env before tests run

--- tests/integration/test-utils.js
+++ tests/integration/test-utils.js
@@ -1,29 +1,9 @@
-import { MongoMemoryServer } from 'mongodb-memory-server'
 import { createServer } from '../../src/server.js'
 import { config } from '../../src/config/config.js'
 
-let mongod
 let server
 
 export const setupTestEnvironment = async () => {
-  const dbName = 'testdb'
-
-  mongod = await MongoMemoryServer.create({
-    instance: {
-      dbName
-    },
-    binary: {
-      version: '7.0.0'
-    }
-  })
-  const mongoUri = mongod.getUri()
-
-  process.env.MONGO_URI = mongoUri
-  process.env.MONGO_DATABASE = dbName
-
-  config.set('mongo.mongoUrl', mongoUri)
-  config.set('mongo.databaseName', dbName)
-
   server = await createServer()
 
   return server
@@ -32,8 +12,5 @@ export const setupTestEnvironment = async () => {
 export const teardownTestEnvironment = async () => {
   if (server) {
     await server.stop()
-  }
-  if (mongod) {
-    await mongod.stop()
   }
 }
