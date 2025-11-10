0a99535e320816b892ced36ca95511ab255a5711
rossjarvisesynergy <ross.jarvis@esynergy.co.uk>
2025-11-10 11:41:29 +0000

    fix(AHWR-1366): set https proxy env variable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import process from 'node:process'
 import { getLogger } from './logging/logger.js'
 import { startServer } from './common/helpers/start-server.js'
 
+//TODO config
+process.env.HTTPS_PROXY = process.env.HTTP_PROXY
+
 await startServer()
 
 process.on('unhandledRejection', (error) => {
