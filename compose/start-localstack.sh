15c8e8f2589464ebe6510f8add8ad820a422a83c
Rob Catton <robmcatton@gmail.com>
2025-11-27 12:10:15 +0000

    Added s3 support to localstack script

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -4,6 +4,29 @@ export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 
+function create_bucket() {
+  local bucket_name=$1
+  echo "Creating S3 bucket: $bucket_name"
+  awslocal s3api create-bucket \
+  --bucket "$bucket_name" \
+  --create-bucket-configuration LocationConstraint=eu-west-2
+}
+
+function upload_pdf() {
+  local bucket_name="local-ahwr-documents"
+  local key_path="123456789/IAHW-G3CL-V59P.pdf"
+  local local_path="/etc/localstack/init/fixtures/testing-doc.pdf"
+
+  if [[ ! -f "$local_path" ]]; then
+  echo "PDF not found at: $local_path"
+  exit 1
+  fi
+
+  echo "Uploading $local_path to s3://$bucket_name/$key_path"
+
+  awslocal s3 cp "$local_path" "s3://$bucket_name/$key_path"
+}
+
 function create_topic() {
   local topic_name=$1
   local topic_arn=$(awslocal sns create-topic --name $topic_name --query "TopicArn" --output text)
@@ -68,14 +91,17 @@ function create_topic_and_queue() {
 create_topic_and_queue "ahwr_payment_update" "ahwr_application_backend_queue" &
 create_topic_and_queue "ahwr_document_request" "ahwr_document_generator_queue" &
 create_topic_and_queue "ahwr_reminder_request" "ahwr_message_generator_queue" &
-create_topic_and_queue "ahwr_status_change" "ahwr_message_generator_queue"
+create_topic_and_queue "ahwr_status_change" "ahwr_message_generator_queue" &
+create_bucket "local-ahwr-documents"
+upload_pdf
 
 wait
 
 awslocal sqs list-queues
 awslocal sns list-topics
+awslocal s3api list-buckets
 
-echo "SNS/SQS ready"
+echo "SNS/SQS/S3 ready"
 
 # Set claim to paid
 awslocal sns publish --topic-arn arn:aws:sns:eu-west-2:000000000000:ahwr_payment_update --message '{"sbi":"123456789","claimRef":"FUBC-JTTU-SDQ7"}' --message-attributes '{"eventType":{"DataType":"String","StringValue":"uk.gov.ffc.ahwr.set.paid.status"}}'
