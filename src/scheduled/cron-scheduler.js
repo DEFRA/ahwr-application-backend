a9975dc5b46821ec98769b496ca99654563ecda4
Rob Catton <robmcatton@gmail.com>
2025-12-31 13:56:34 +0000

    Fixing log message

--- src/scheduled/cron-scheduler.js
+++ src/scheduled/cron-scheduler.js
@@ -9,7 +9,7 @@ const jobs = {
 }
 
 const buildMongoUri = (baseUri, databaseName) => {
-  getLogger().info({ baseUri })
+  getLogger().info(baseUri)
   const [uriWithoutQuery, query] = baseUri.split('?', 2)
 
   const normalisedBase = uriWithoutQuery.endsWith('/')
