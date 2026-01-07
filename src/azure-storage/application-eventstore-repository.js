634bc9a59ac96b076fccc1d6d892b6ec5d4c99de
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-01-07 16:09:26 +0000

    feat(AHWR-1468): until implemented, satisfy sonarqube

--- src/azure-storage/application-eventstore-repository.js
+++ src/azure-storage/application-eventstore-repository.js
@@ -1,6 +1,6 @@
 // import { REDACT_PII_VALUES } from 'ffc-ahwr-common-library'
 
-export const redactPII = async (sbi, logger, startDate, endDate) => {
+export const redactPII = async (sbi, logger, _startDate, _endDate) => {
   // TODO 1182 impl
   // const propertiesToMerge = {
   //   ChangedBy: REDACT_PII_VALUES.REDACTED_CHANGED_BY,
