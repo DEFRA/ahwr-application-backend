807ab67c7f10632f4282ea3c9f113cd3f94712b6
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-01-07 16:09:26 +0000

    feat(AHWR-1468): until implemented, satisfy sonarqube

--- src/azure-storage/application-ineligibility-repository.js
+++ src/azure-storage/application-ineligibility-repository.js
@@ -3,7 +3,7 @@
 
 // TODO - is this to delete?
 
-export const redactPII = async (sbi, logger, startDate, endDate) => {
+export const redactPII = async (sbi, logger, _startDate, _endDate) => {
   // TODO 1182 impl
   // const propertiesToMerge = { ChangedBy: REDACT_PII_VALUES.REDACTED_CHANGED_BY }
   // await updateEntitiesByPartitionKey(
