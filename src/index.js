25893a1f021d27314f75abd2f964e22d7611701b
Aaron Carroll <20041831+aaroncarroll82@users.noreply.github.com>
2026-02-25 11:51:33 +0000

    fix(AHWR-1602): On unhandled rejection, exit app #patch (#124)
    * fix(AHWR-1602): On unhandled rejection, exit app #patch

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import { stopFcpMessagingService } from './messaging/fcp-messaging-service.js'
 import { getLogger } from './logging/logger.js'
 import { startServer } from './common/helpers/start-server.js'
 
-//TODO config
 process.env.HTTPS_PROXY = process.env.HTTP_PROXY
 
 await startServer()
 
 process.on('unhandledRejection', async (error) => {
   const logger = getLogger()
-  logger.info('Unhandled rejection')
-  logger.error(error)
+  logger.error(error, 'Unhandled rejection')
   await stopFcpMessagingService()
 
-  process.exitCode = 1
+  process.exit(1)
 })
