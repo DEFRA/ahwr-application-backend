7ae6fdd043796970639baefae7761fa2d045d115
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-03-17 15:35:43 +0000

    Adding test for poultry. Removing unneeded mock

--- src/routes/api/claims/claims-service.test.js
+++ src/routes/api/claims/claims-service.test.js
@@ -8,7 +8,7 @@ import {
   isURNUnique as isNWURNUnique,
   getClaimByReference
 } from '../../../repositories/claim-repository.js'
-import { createClaimReference } from '../../../lib/create-reference.js'
+import * as createReference from '../../../lib/create-reference.js'
 import { validateClaim } from '../../../processing/claim/validation.js'
 import {
   saveClaimAndRelatedData,
@@ -20,7 +20,6 @@ import { trackError } from '../../../logging/logger.js'
 jest.mock('../../../repositories/application-repository.js')
 jest.mock('../../../repositories/claim-repository.js')
 jest.mock('../../../repositories/ow-application-repository.js')
-jest.mock('../../../lib/create-reference.js')
 jest.mock('../../../processing/claim/validation.js')
 jest.mock('../../../processing/claim/ahwr/processor.js')
 jest.mock('../../../logging/logger.js')
@@ -30,180 +29,291 @@ jest.mock('@hapi/boom', () => ({
 }))
 
 describe('processClaim', () => {
-  const mockLogger = { setBindings: jest.fn(), error: jest.fn() }
-  const mockDb = {}
-  const payload = {
-    applicationReference: 'IAHW-AAAA-AAAA',
-    type: 'REVIEW',
-    reference: 'TEMP-O9UD-0025',
-    data: {
-      typeOfLivestock: 'beef',
-      laboratoryURN: 'AK-2024-38',
-      herd: {
-        id: 'db32152a-724a-4c5d-8073-0901c8d307f7'
+  describe('poultry claim', () => {
+    const mockLogger = { setBindings: jest.fn(), error: jest.fn() }
+    const mockDb = {}
+    const payload = {
+      applicationReference: 'POUL-AAAA-AAAA',
+      type: 'REVIEW',
+      reference: 'TEMP-CLAIM-O9UD-0025',
+      data: {
+        typeOfLivestock: 'broilers',
+        herd: {
+          id: 'db32152a-724a-4c5d-8073-0901c8d307f7'
+        }
       }
     }
-  }
 
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
+    beforeEach(() => {
+      jest.spyOn(createReference, 'createClaimReference')
+    })
 
-  const mockIsURNNumberUnique = (unique) => {
-    getApplicationsBySbi.mockResolvedValue([{ reference: 'IAHW-7NF8-3KB9' }])
-    isNWURNUnique.mockResolvedValue(unique)
-    isOWURNUnique.mockResolvedValue(true)
-  }
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
 
-  test('creates and returns claim when valid request', async () => {
-    const herdData = {
-      id: 'db32152a-724a-4c5d-8073-0901c8d307f7',
-      version: 1,
-      cph: '12/345/6789',
-      name: 'Sheep herd 2',
-      reasons: ['uniqueHealthNeeds'],
-      associatedAt: '2025-10-21T09:28:49.760Z'
-    }
-    const saveClaimResult = {
-      claim: {
-        applicationReference: 'IAHW-AAAA-AAAA',
-        reference: 'RESH-O9UD-0025',
-        data: {
-          typeOfLivestock: 'sheep',
-          dateOfVisit: '2025-10-20T00:00:00.000Z',
-          dateOfTesting: '2024-01-22T00:00:00.000Z',
-          vetsName: 'Jane Doe',
-          vetRCVSNumber: 'AK-2024',
-          laboratoryURN: 'AK-2024-38',
-          numberAnimalsTested: 30,
-          speciesNumbers: 'yes',
-          amount: 4,
-          claimType: 'REVIEW'
+    test('creates and returns claim when valid request', async () => {
+      const herdData = {
+        id: 'db32152a-724a-4c5d-8073-0901c8d307f7',
+        version: 1,
+        cph: '12/345/6789',
+        name: 'Broilers 2',
+        associatedAt: '2025-10-21T09:28:49.760Z'
+      }
+      const saveClaimResult = {
+        claim: {
+          applicationReference: 'POUL-AAAA-AAAA',
+          reference: 'POBR-O9UD-0025',
+          data: {
+            typeOfLivestock: 'broilers',
+            dateOfVisit: '2025-10-20T00:00:00.000Z',
+            dateOfTesting: '2024-01-22T00:00:00.000Z',
+            vetsName: 'Jane Doe',
+            vetRCVSNumber: 'AK-2024',
+            laboratoryURN: 'AK-2024-38',
+            numberAnimalsTested: 30,
+            speciesNumbers: 'yes',
+            amount: 4,
+            claimType: 'REVIEW'
+          },
+          type: 'REVIEW',
+          createdBy: 'admin',
+          status: 'ON_HOLD',
+          herd: {
+            id: 'db32152a-724a-4c5d-8073-0901c8d307f7',
+            version: 1,
+            cph: '12/345/6789',
+            name: 'Sheep herd 2',
+            reasons: ['uniqueHealthNeeds'],
+            associatedAt: '2025-10-21T09:28:49.760Z'
+          }
         },
-        type: 'REVIEW',
-        createdBy: 'admin',
-        status: 'ON_HOLD',
+        isMultiHerdsClaim: true,
+        herdGotUpdated: true,
+        herdData
+      }
+      const application = {
+        flags: [],
+        organisation: { sbi: '123456789' }
+      }
+      getApplication.mockResolvedValue(application)
+      validateClaim.mockReturnValue({ value: payload })
+      saveClaimAndRelatedData.mockResolvedValue(saveClaimResult)
+
+      const result = await processClaim({
+        payload,
+        logger: mockLogger,
+        db: mockDb
+      })
+
+      expect(result).toEqual(saveClaimResult.claim)
+      expect(createReference.createClaimReference).toHaveBeenCalledWith(
+        'TEMP-CLAIM-O9UD-0025',
+        'REVIEW',
+        'broilers',
+        true
+      )
+      expect(saveClaimAndRelatedData).toHaveBeenCalledWith({
+        db: mockDb,
+        sbi: '123456789',
+        claimPayload: payload,
+        claimReference: 'POBR-O9UD-0025',
+        flags: [],
+        logger: mockLogger
+      })
+      expect(generateEventsAndComms).toHaveBeenCalledWith(
+        true,
+        saveClaimResult.claim,
+        application,
+        herdData,
+        true,
+        'db32152a-724a-4c5d-8073-0901c8d307f7'
+      )
+    })
+  })
+
+  describe('standard herd claim', () => {
+    const mockLogger = { setBindings: jest.fn(), error: jest.fn() }
+    const mockDb = {}
+    const payload = {
+      applicationReference: 'IAHW-AAAA-AAAA',
+      type: 'REVIEW',
+      reference: 'TEMP-CLAIM-O9UD-0025',
+      data: {
+        typeOfLivestock: 'beef',
+        laboratoryURN: 'AK-2024-38',
         herd: {
-          id: 'db32152a-724a-4c5d-8073-0901c8d307f7',
-          version: 1,
-          cph: '12/345/6789',
-          name: 'Sheep herd 2',
-          reasons: ['uniqueHealthNeeds'],
-          associatedAt: '2025-10-21T09:28:49.760Z'
+          id: 'db32152a-724a-4c5d-8073-0901c8d307f7'
         }
-      },
-      isMultiHerdsClaim: true,
-      herdGotUpdated: true,
-      herdData
+      }
     }
-    const application = {
-      flags: [],
-      organisation: { sbi: '123456789' }
+
+    beforeEach(() => {
+      jest.spyOn(createReference, 'createClaimReference')
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    const mockIsURNNumberUnique = (unique) => {
+      getApplicationsBySbi.mockResolvedValue([{ reference: 'IAHW-7NF8-3KB9' }])
+      isNWURNUnique.mockResolvedValue(unique)
+      isOWURNUnique.mockResolvedValue(true)
     }
-    getApplication.mockResolvedValue(application)
-    validateClaim.mockReturnValue({ value: payload })
-    createClaimReference.mockReturnValue('RESH-O9UD-0025')
-    mockIsURNNumberUnique(true)
-    saveClaimAndRelatedData.mockResolvedValue(saveClaimResult)
 
-    const result = await processClaim({
-      payload,
-      logger: mockLogger,
-      db: mockDb
+    test('creates and returns claim when valid request', async () => {
+      const herdData = {
+        id: 'db32152a-724a-4c5d-8073-0901c8d307f7',
+        version: 1,
+        cph: '12/345/6789',
+        name: 'Sheep herd 2',
+        reasons: ['uniqueHealthNeeds'],
+        associatedAt: '2025-10-21T09:28:49.760Z'
+      }
+      const saveClaimResult = {
+        claim: {
+          applicationReference: 'IAHW-AAAA-AAAA',
+          reference: 'REBC-O9UD-0025',
+          data: {
+            typeOfLivestock: 'sheep',
+            dateOfVisit: '2025-10-20T00:00:00.000Z',
+            dateOfTesting: '2024-01-22T00:00:00.000Z',
+            vetsName: 'Jane Doe',
+            vetRCVSNumber: 'AK-2024',
+            laboratoryURN: 'AK-2024-38',
+            numberAnimalsTested: 30,
+            speciesNumbers: 'yes',
+            amount: 4,
+            claimType: 'REVIEW'
+          },
+          type: 'REVIEW',
+          createdBy: 'admin',
+          status: 'ON_HOLD',
+          herd: {
+            id: 'db32152a-724a-4c5d-8073-0901c8d307f7',
+            version: 1,
+            cph: '12/345/6789',
+            name: 'Sheep herd 2',
+            reasons: ['uniqueHealthNeeds'],
+            associatedAt: '2025-10-21T09:28:49.760Z'
+          }
+        },
+        isMultiHerdsClaim: true,
+        herdGotUpdated: true,
+        herdData
+      }
+      const application = {
+        flags: [],
+        organisation: { sbi: '123456789' }
+      }
+      getApplication.mockResolvedValue(application)
+      validateClaim.mockReturnValue({ value: payload })
+      mockIsURNNumberUnique(true)
+      saveClaimAndRelatedData.mockResolvedValue(saveClaimResult)
+
+      const result = await processClaim({
+        payload,
+        logger: mockLogger,
+        db: mockDb
+      })
+
+      expect(result).toEqual(saveClaimResult.claim)
+      expect(createReference.createClaimReference).toHaveBeenCalledWith(
+        'TEMP-CLAIM-O9UD-0025',
+        'REVIEW',
+        'beef',
+        false
+      )
+      expect(saveClaimAndRelatedData).toHaveBeenCalledWith({
+        db: mockDb,
+        sbi: '123456789',
+        claimPayload: payload,
+        claimReference: 'REBC-O9UD-0025',
+        flags: [],
+        logger: mockLogger
+      })
+      expect(generateEventsAndComms).toHaveBeenCalledWith(
+        true,
+        saveClaimResult.claim,
+        application,
+        herdData,
+        true,
+        'db32152a-724a-4c5d-8073-0901c8d307f7'
+      )
     })
 
-    expect(result).toEqual(saveClaimResult.claim)
-    expect(createClaimReference).toHaveBeenCalledWith('TEMP-O9UD-0025', 'REVIEW', 'beef', false)
-    expect(saveClaimAndRelatedData).toHaveBeenCalledWith({
-      db: mockDb,
-      sbi: '123456789',
-      claimPayload: payload,
-      claimReference: 'RESH-O9UD-0025',
-      flags: [],
-      logger: mockLogger
+    test('throws NotFound error when application does not exist', async () => {
+      getApplication.mockResolvedValue(null)
+
+      await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
+        'NotFound'
+      )
+
+      expect(getApplication).toHaveBeenCalledWith({
+        db: mockDb,
+        reference: 'IAHW-AAAA-AAAA'
+      })
     })
-    expect(generateEventsAndComms).toHaveBeenCalledWith(
-      true,
-      saveClaimResult.claim,
-      application,
-      herdData,
-      true,
-      'db32152a-724a-4c5d-8073-0901c8d307f7'
-    )
-  })
 
-  test('throws NotFound error when application does not exist', async () => {
-    getApplication.mockResolvedValue(null)
+    test('throws BadRequest when request is invalid', async () => {
+      getApplication.mockResolvedValue({
+        flags: [],
+        organisation: { sbi: '123456789' }
+      })
+      const validationError = new Error('Invalid claim')
+      validateClaim.mockReturnValue({ error: validationError })
 
-    await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
-      'NotFound'
-    )
+      await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
+        'BadRequest'
+      )
 
-    expect(getApplication).toHaveBeenCalledWith({
-      db: mockDb,
-      reference: 'IAHW-AAAA-AAAA'
+      expect(validateClaim).toHaveBeenCalledWith(AHWR_SCHEME, payload, [])
+      expect(trackError).toHaveBeenCalledWith(
+        mockLogger,
+        validationError,
+        'failed-validation',
+        'Create claim validation error'
+      )
     })
-  })
 
-  test('throws BadRequest when request is invalid', async () => {
-    getApplication.mockResolvedValue({
-      flags: [],
-      organisation: { sbi: '123456789' }
+    test('throws BadRequest when URN number is not unique', async () => {
+      getApplication.mockResolvedValue({
+        flags: [],
+        organisation: { sbi: '123456789' }
+      })
+      validateClaim.mockReturnValue({ value: payload })
+      mockIsURNNumberUnique(false)
+
+      await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
+        'BadRequest'
+      )
+
+      expect(isNWURNUnique).toHaveBeenCalledWith({
+        db: mockDb,
+        laboratoryURN: 'AK-2024-38',
+        applicationReferences: ['IAHW-7NF8-3KB9']
+      })
+      expect(isOWURNUnique).toHaveBeenCalledWith({
+        db: mockDb,
+        sbi: '123456789',
+        laboratoryURN: 'AK-2024-38'
+      })
     })
-    const validationError = new Error('Invalid claim')
-    validateClaim.mockReturnValue({ error: validationError })
 
-    await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
-      'BadRequest'
-    )
+    test('throws error when claim was not created', async () => {
+      getApplication.mockResolvedValue({
+        flags: [],
+        organisation: { sbi: '123456789' }
+      })
+      validateClaim.mockReturnValue({ value: payload })
+      mockIsURNNumberUnique(true)
+      saveClaimAndRelatedData.mockResolvedValue({ claim: null })
 
-    expect(validateClaim).toHaveBeenCalledWith(AHWR_SCHEME, payload, [])
-    expect(trackError).toHaveBeenCalledWith(
-      mockLogger,
-      validationError,
-      'failed-validation',
-      'Create claim validation error'
-    )
-  })
-
-  test('throws BadRequest when URN number is not unique', async () => {
-    getApplication.mockResolvedValue({
-      flags: [],
-      organisation: { sbi: '123456789' }
+      await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
+        'Claim was not created'
+      )
     })
-    validateClaim.mockReturnValue({ value: payload })
-    createClaimReference.mockReturnValue('RESH-O9UD-0025')
-    mockIsURNNumberUnique(false)
-
-    await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
-      'BadRequest'
-    )
-
-    expect(isNWURNUnique).toHaveBeenCalledWith({
-      db: mockDb,
-      laboratoryURN: 'AK-2024-38',
-      applicationReferences: ['IAHW-7NF8-3KB9']
-    })
-    expect(isOWURNUnique).toHaveBeenCalledWith({
-      db: mockDb,
-      sbi: '123456789',
-      laboratoryURN: 'AK-2024-38'
-    })
-  })
-
-  test('throws error when claim was not created', async () => {
-    getApplication.mockResolvedValue({
-      flags: [],
-      organisation: { sbi: '123456789' }
-    })
-    validateClaim.mockReturnValue({ value: payload })
-    createClaimReference.mockReturnValue('RESH-O9UD-0025')
-    mockIsURNNumberUnique(true)
-    saveClaimAndRelatedData.mockResolvedValue({ claim: null })
-
-    await expect(processClaim({ payload, logger: mockLogger, db: mockDb })).rejects.toThrow(
-      'Claim was not created'
-    )
   })
 })
 
