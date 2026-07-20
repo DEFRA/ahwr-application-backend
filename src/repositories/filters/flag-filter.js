c5cc98be4774e185c12a95b3b2b99b7b0430cd25
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2026-07-20 17:07:47 +0100

    Fixing sonar issue

--- src/repositories/filters/flag-filter.js
+++ src/repositories/filters/flag-filter.js
@@ -1,10 +1,11 @@
 const hasNonDeletedFlag = { $elemMatch: { deleted: { $ne: true } } }
 
 export const applyFlagFilter = (query, flag) => {
-  // Skip when flag is 'ALL'/absent or an unknown value
   if (flag === 'FLAGGED') {
     query.flags = hasNonDeletedFlag
   } else if (flag === 'NOT_FLAGGED') {
     query.flags = { $not: hasNonDeletedFlag }
+  } else {
+    // Skip when flag is 'ALL'/absent or an unknown value
   }
 }
